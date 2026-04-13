c4e2fdc7bb53c46ea75295f914270c75e37f5b25
Kahina Khacef <kahina.khacef@ey.com>
2026-04-13 16:41:50 +0100

    test(api): cover proposers route success and error paths

--- nightfall_client/src/drivers/rest/proposers.rs
+++ nightfall_client/src/drivers/rest/proposers.rs
@@ -1,5 +1,6 @@
 use crate::domain::entities::Proposer;
 use configuration::addresses::get_addresses;
+use futures::Future;
 use lib::{
     blockchain_client::BlockchainClientConnection, initialisation::get_blockchain_client_connection,
 };
@@ -14,6 +15,19 @@ pub fn get_proposers() -> impl Filter<Extract = impl warp::Reply, Error = warp::
 }
 
 async fn handle_get_proposers() -> Result<impl Reply, warp::Rejection> {
+    handle_get_proposers_with(fetch_proposers).await
+}
+
+async fn handle_get_proposers_with<F, Fut>(fetch: F) -> Result<impl Reply, warp::Rejection>
+where
+    F: FnOnce() -> Fut,
+    Fut: Future<Output = Result<Vec<Proposer>, warp::Rejection>>,
+{
+    let list = fetch().await?;
+    Ok(reply::json(&list))
+}
+
+async fn fetch_proposers() -> Result<Vec<Proposer>, warp::Rejection> {
     // get a ManageProposers instance
     let blockchain_client = get_blockchain_client_connection()
         .await
@@ -38,5 +52,69 @@ async fn handle_get_proposers() -> Result<impl Reply, warp::Rejection> {
             previous_addr: p.previous_addr,
         })
         .collect::<Vec<Proposer>>();
-    Ok(reply::json(&list))
+    Ok(list)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use alloy::primitives::{Address, U256};
+    use serde_json::Value;
+    use warp::{http::StatusCode, Filter};
+
+    fn sample_proposers() -> Vec<Proposer> {
+        vec![Proposer {
+            stake: U256::from(42u64),
+            addr: Address::from([0x11; 20]),
+            url: "http://proposer-1.example".to_string(),
+            next_addr: Address::from([0x22; 20]),
+            previous_addr: Address::from([0x33; 20]),
+        }]
+    }
+
+    #[tokio::test]
+    async fn test_proposers_route_returns_ok_with_json_array() {
+        let filter = path!("v1" / "proposers")
+            .and(warp::get())
+            .and_then(|| handle_get_proposers_with(|| async { Ok(sample_proposers()) }));
+
+        let res = warp::test::request()
+            .method("GET")
+            .path("/v1/proposers")
+            .reply(&filter)
+            .await;
+
+        assert_eq!(res.status(), StatusCode::OK);
+        let body = serde_json::from_slice::<Value>(res.body()).expect("body should be JSON");
+        assert!(body.is_array(), "expected JSON array");
+        assert_eq!(body.as_array().unwrap().len(), 1);
+        assert_eq!(body[0]["url"], "http://proposer-1.example");
+    }
+
+    #[tokio::test]
+    async fn test_proposers_route_returns_service_unavailable_on_provider_error() {
+        let filter = path!("v1" / "proposers")
+            .and(warp::get())
+            .and_then(|| async {
+                handle_get_proposers_with(|| async {
+                    Err(warp::reject::custom(
+                        crate::domain::error::ClientRejection::ProposerError,
+                    ))
+                })
+                .await
+            })
+            .recover(super::super::handle_rejection);
+
+        let res = warp::test::request()
+            .method("GET")
+            .path("/v1/proposers")
+            .reply(&filter)
+            .await;
+
+        assert_eq!(res.status(), StatusCode::SERVICE_UNAVAILABLE);
+        assert_eq!(
+            std::str::from_utf8(res.body()).unwrap(),
+            "Failed to get list of Proposers"
+        );
+    }
 }
