e00de42c91de1781faed80570d3bcec913397b83
Kahina Khacef <kahina.khacef@ey.com>
2026-04-17 13:35:40 +0100

    Simplify balance handler test hooks

--- nightfall_client/src/drivers/rest/balance.rs
+++ nightfall_client/src/drivers/rest/balance.rs
@@ -4,12 +4,10 @@ use crate::{
 use alloy::primitives::U256;
 use ark_ff::{BigInteger, PrimeField};
 use lib::{
-    blockchain_client::BlockchainClientConnection, get_fee_token_id,
-    hex_conversion::HexConvertible, initialisation::get_blockchain_client_connection,
-    nf_token_id::to_nf_token_id_from_str,
+    blockchain_client::BlockchainClientConnection, get_fee_token_id, hex_conversion::HexConvertible,
+    initialisation::get_blockchain_client_connection, nf_token_id::to_nf_token_id_from_str,
 };
 use std::future::Future;
-use std::pin::Pin;
 use warp::{http::StatusCode, path, reply::Reply, Filter};
 
 fn encode_balance_hex(balance: ark_bn254::Fr) -> String {
@@ -61,7 +59,13 @@ pub fn get_fee_balance(
 }
 
 pub async fn handle_get_fee_balance() -> Result<impl Reply, warp::Rejection> {
-    handle_get_fee_balance_with(current_fee_balance_fetcher()).await
+    handle_get_fee_balance_with(|| async {
+        // Get the fee token ID from the configured contract addresses and load its balance from the DB.
+        let fee_token_id = get_fee_token_id();
+        let db = get_db_connection().await;
+        db.get_balance(&fee_token_id).await
+    })
+    .await
 }
 
 /// Endpoint to get the L1 balance of the client's wallet
@@ -71,57 +75,6 @@ pub fn get_l1_balance(
     path!("v1" / "l1_balance")
         .and(warp::get())
         .and_then(handle_get_l1_balance)
-}
-
-type L1BalanceFuture = Pin<Box<dyn Future<Output = Option<U256>> + Send>>;
-type L1BalanceFetcher = fn() -> L1BalanceFuture;
-type FeeBalanceFuture = Pin<Box<dyn Future<Output = Option<ark_bn254::Fr>> + Send>>;
-type FeeBalanceFetcher = fn() -> FeeBalanceFuture;
-
-fn default_fee_balance_fetcher() -> FeeBalanceFuture {
-    Box::pin(async {
-        // Get the fee token ID from the configured contract addresses and load its balance from the DB.
-        let fee_token_id = get_fee_token_id();
-        let db = get_db_connection().await;
-        db.get_balance(&fee_token_id).await
-    })
-}
-
-#[cfg(test)]
-fn current_fee_balance_fetcher() -> FeeBalanceFetcher {
-    let override_fetcher = test_support::get_fee_balance_fetcher_override()
-        .lock()
-        .expect("test fetcher lock should not be poisoned")
-        .as_ref()
-        .copied();
-    override_fetcher.unwrap_or(default_fee_balance_fetcher as FeeBalanceFetcher)
-}
-
-#[cfg(not(test))]
-fn current_fee_balance_fetcher() -> FeeBalanceFetcher {
-    default_fee_balance_fetcher
-}
-
-fn default_l1_balance_fetcher() -> L1BalanceFuture {
-    Box::pin(async {
-        let client = get_blockchain_client_connection().await.read().await;
-        client.get_balance().await
-    })
-}
-
-#[cfg(test)]
-fn current_l1_balance_fetcher() -> L1BalanceFetcher {
-    let override_fetcher = test_support::get_l1_balance_fetcher_override()
-        .lock()
-        .expect("test fetcher lock should not be poisoned")
-        .as_ref()
-        .copied();
-    override_fetcher.unwrap_or(default_l1_balance_fetcher as L1BalanceFetcher)
-}
-
-#[cfg(not(test))]
-fn current_l1_balance_fetcher() -> L1BalanceFetcher {
-    default_l1_balance_fetcher
 }
 
 async fn handle_get_l1_balance_with<F, Fut>(fetch_balance: F) -> Result<impl Reply, warp::Rejection>
@@ -154,35 +107,11 @@ where
 
 pub async fn handle_get_l1_balance() -> Result<impl Reply, warp::Rejection> {
     // Read the current wallet balance from the shared blockchain client.
-    handle_get_l1_balance_with(current_l1_balance_fetcher()).await
-}
-
-#[cfg(test)]
-mod test_support {
-    use super::*;
-    use std::sync::{Mutex, OnceLock};
-    use tokio::sync::Mutex as AsyncMutex;
-
-    static L1_BALANCE_FETCHER_OVERRIDE: OnceLock<Mutex<Option<L1BalanceFetcher>>> = OnceLock::new();
-    static FEE_BALANCE_FETCHER_OVERRIDE: OnceLock<Mutex<Option<FeeBalanceFetcher>>> = OnceLock::new();
-    static L1_BALANCE_TEST_LOCK: OnceLock<AsyncMutex<()>> = OnceLock::new();
-    static FEE_BALANCE_TEST_LOCK: OnceLock<AsyncMutex<()>> = OnceLock::new();
-
-    pub(super) fn get_l1_balance_fetcher_override() -> &'static Mutex<Option<L1BalanceFetcher>> {
-        L1_BALANCE_FETCHER_OVERRIDE.get_or_init(|| Mutex::new(None))
-    }
-
-    pub(super) fn get_fee_balance_fetcher_override() -> &'static Mutex<Option<FeeBalanceFetcher>> {
-        FEE_BALANCE_FETCHER_OVERRIDE.get_or_init(|| Mutex::new(None))
-    }
-
-    pub(super) fn get_l1_balance_test_lock() -> &'static AsyncMutex<()> {
-        L1_BALANCE_TEST_LOCK.get_or_init(|| AsyncMutex::new(()))
-    }
-
-    pub(super) fn get_fee_balance_test_lock() -> &'static AsyncMutex<()> {
-        FEE_BALANCE_TEST_LOCK.get_or_init(|| AsyncMutex::new(()))
-    }
+    handle_get_l1_balance_with(|| async {
+        let client = get_blockchain_client_connection().await.read().await;
+        client.get_balance().await
+    })
+    .await
 }
 
 #[cfg(test)]
@@ -191,32 +120,25 @@ mod tests {
     use warp::hyper::body::to_bytes;
     use warp::{http::StatusCode, Reply};
 
-    fn fetch_some_balance() -> L1BalanceFuture {
-        Box::pin(async { Some(U256::from(0x123u64)) })
+    async fn fetch_some_balance() -> Option<U256> {
+        Some(U256::from(0x123u64))
     }
 
-    fn fetch_no_balance() -> L1BalanceFuture {
-        Box::pin(async { None })
+    async fn fetch_no_balance() -> Option<U256> {
+        None
     }
 
-    fn fetch_some_fee_balance() -> FeeBalanceFuture {
-        Box::pin(async { Some(ark_bn254::Fr::from(3u64)) })
+    async fn fetch_some_fee_balance() -> Option<ark_bn254::Fr> {
+        Some(ark_bn254::Fr::from(3u64))
     }
 
-    fn fetch_no_fee_balance() -> FeeBalanceFuture {
-        Box::pin(async { None })
+    async fn fetch_no_fee_balance() -> Option<ark_bn254::Fr> {
+        None
     }
 
     #[tokio::test]
-    async fn test_handle_l1_balance_returns_hex_balance() {
-        let _guard = test_support::get_l1_balance_test_lock()
-            .lock()
-            .await;
-        *test_support::get_l1_balance_fetcher_override()
-            .lock()
-            .expect("test fetcher lock should not be poisoned") = Some(fetch_some_balance);
-
-        let res = handle_get_l1_balance().await;
+    async fn test_handle_l1_balance_with_returns_hex_balance() {
+        let res = handle_get_l1_balance_with(fetch_some_balance).await;
         let response = res.unwrap().into_response();
         let status = response.status();
         let body = to_bytes(response.into_body()).await.unwrap();
@@ -226,22 +148,11 @@ mod tests {
             std::str::from_utf8(&body).unwrap(),
             U256::from(0x123u64).to_hex_string()
         );
-
-        *test_support::get_l1_balance_fetcher_override()
-            .lock()
-            .expect("test fetcher lock should not be poisoned") = None;
     }
 
     #[tokio::test]
-    async fn test_handle_l1_balance_returns_not_found_when_balance_unavailable() {
-        let _guard = test_support::get_l1_balance_test_lock()
-            .lock()
-            .await;
-        *test_support::get_l1_balance_fetcher_override()
-            .lock()
-            .expect("test fetcher lock should not be poisoned") = Some(fetch_no_balance);
-
-        let err = match handle_get_l1_balance().await {
+    async fn test_handle_l1_balance_with_returns_not_found_when_balance_unavailable() {
+        let err = match handle_get_l1_balance_with(fetch_no_balance).await {
             Ok(_) => panic!("missing balance should return a rejection"),
             Err(err) => err,
         };
@@ -257,22 +168,11 @@ mod tests {
             std::str::from_utf8(&body).unwrap(),
             "No such token"
         );
-
-        *test_support::get_l1_balance_fetcher_override()
-            .lock()
-            .expect("test fetcher lock should not be poisoned") = None;
     }
 
     #[tokio::test]
-    async fn test_handle_fee_balance_returns_hex_balance() {
-        let _guard = test_support::get_fee_balance_test_lock()
-            .lock()
-            .await;
-        *test_support::get_fee_balance_fetcher_override()
-            .lock()
-            .expect("test fetcher lock should not be poisoned") = Some(fetch_some_fee_balance);
-
-        let res = handle_get_fee_balance().await;
+    async fn test_handle_fee_balance_with_returns_hex_balance() {
+        let res = handle_get_fee_balance_with(fetch_some_fee_balance).await;
         let response = res.unwrap().into_response();
         let status = response.status();
         let body = to_bytes(response.into_body()).await.unwrap();
@@ -282,22 +182,11 @@ mod tests {
             std::str::from_utf8(&body).unwrap(),
             ark_bn254::Fr::from(3u64).to_hex_string()
         );
-
-        *test_support::get_fee_balance_fetcher_override()
-            .lock()
-            .expect("test fetcher lock should not be poisoned") = None;
     }
 
     #[tokio::test]
-    async fn test_handle_fee_balance_returns_not_found_when_balance_unavailable() {
-        let _guard = test_support::get_fee_balance_test_lock()
-            .lock()
-            .await;
-        *test_support::get_fee_balance_fetcher_override()
-            .lock()
-            .expect("test fetcher lock should not be poisoned") = Some(fetch_no_fee_balance);
-
-        let err = match handle_get_fee_balance().await {
+    async fn test_handle_fee_balance_with_returns_not_found_when_balance_unavailable() {
+        let err = match handle_get_fee_balance_with(fetch_no_fee_balance).await {
             Ok(_) => panic!("missing fee balance should return a rejection"),
             Err(err) => err,
         };
@@ -310,9 +199,5 @@ mod tests {
 
         assert_eq!(status, StatusCode::NOT_FOUND);
         assert_eq!(std::str::from_utf8(&body).unwrap(), "No such token");
-
-        *test_support::get_fee_balance_fetcher_override()
-            .lock()
-            .expect("test fetcher lock should not be poisoned") = None;
     }
 }
